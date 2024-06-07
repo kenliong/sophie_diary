db7bf4ec416d6ddfaea7701e0b615686af6358f8
clara <chongcjy@sph.com.sg>
2024-06-07 23:56:49 +0800

    chore: added precommit

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,8 @@
 from utils import *
 
-st.set_page_config(page_title='Template Chatbot', page_icon='💬', layout="wide", initial_sidebar_state="expanded")
+st.set_page_config(
+    page_title="Template Chatbot", page_icon="💬", layout="wide", initial_sidebar_state="expanded"
+)
 st.markdown(get_custom_css_modifier(), unsafe_allow_html=True)
 st.markdown("<h5 style='text-align: Left;'>💬 Template Chatbot</h5>", unsafe_allow_html=True)
 st.title("Sophie's diary")
@@ -16,14 +18,12 @@ with chat_history_box:
 
 prompt = st.chat_input()
 if prompt:
-
     st.chat_message("user").write(prompt)
-    
-    response = f'I hear you said {prompt}'
+
+    response = f"I hear you said {prompt}"
 
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.session_state.messages.append({"role": "assistant", "content": response})
 
     st.chat_message("assistant").write(response)
     st.rerun()
-
--- utils.py
+++ utils.py
@@ -16,4 +16,3 @@ footer {
 </style>
     """
     return css_modifier
-
