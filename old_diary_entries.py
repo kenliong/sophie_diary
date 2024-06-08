e78b7bd9615f854056132405f9c45069a7ffad97
Kenny C <dewei274@gmail.com>
2024-06-08 14:27:23 +0800

    added mental tendencies to data model

--- old_diary_entries.py
+++ old_diary_entries.py
@@ -1,4 +1,24 @@
 import pandas as pd
 
-legacy_df = pd.read_csv("data/journal_entries_v1.csv")
+legacy_df = pd.read_csv("data/journal_entries_v2.csv")
 old_diary_entries = legacy_df['entry_desc'].tolist()
+
+mental_tendencies = [
+'High Expectations',
+'External Validation',
+'Reluctance to Share',
+'Expectation of Reciprocity',
+'Guilt-trapping',
+'Balancing Correction and Encouragement',
+'Demotivation from Initial Failures',
+'Perfectionism',
+'Tunnel Vision',
+'Self-efficacy',
+'Learned Helplessness',
+'Overanalysis',
+'Fear of Failure',
+'Risk Aversion',
+'Impostor Syndrome',
+'Distractibility',
+'Multitasking'
+]
