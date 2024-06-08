839070e3279f3697c5afaff90dfc58dad6f26a79
clara <chongcjy@sph.com.sg>
2024-06-08 22:29:19 +0800

    bug fix

--- new_diary_entry.py
+++ new_diary_entry.py
@@ -1,45 +1,49 @@
-from typing import Optional, List
+from typing import List, Optional
+
+from langchain.output_parsers import PydanticOutputParser
 from pydantic import BaseModel, Field
+
 from utils.llm_utils import *
 
 
 class DeepDiveConversationLabels(BaseModel):
     current_state: Optional[str] = Field(
         default="",
-        description="Label the current state (or real outcome) that this person experienced. If this information is not found, output 'None'."
+        description="Label the current state (or real outcome) that this person experienced. If this information is not found, output 'None'.",
     )
     desired_state: Optional[str] = Field(
         default="",
-        description="Label the desired state (or desired outcome, expectation) that this person expected. If this information is not found, output 'None'"
+        description="Label the desired state (or desired outcome, expectation) that this person expected. If this information is not found, output 'None'",
     )
-    
+
 
 def extract_info_from_conversation(chat_history):
     parser = PydanticOutputParser(pydantic_object=DeepDiveConversationLabels)
-     
-    prompt = f'''
+
+    prompt = f"""
 The following are thoughts from a user, extract the following information.
 {parser.get_format_instructions()}
 ```
 {chat_history}
 ```
-    '''.strip()
-    
+    """.strip()
+
     model = get_llm_instance()
 
-    output = get_completion(model,prompt)
+    output = get_completion(model, prompt)
     parsed_output = parser.parse(output)
-    
+
     return parsed_output
 
-def check_conversation_labels(conversation_labels):
 
-    if not conversation_labels.current_state or conversation_labels.current_state == 'None':
+def check_conversation_labels(conversation_labels):
+    if not conversation_labels.current_state or conversation_labels.current_state == "None":
         return False
-    if not conversation_labels.desired_state or conversation_labels.desired_state == 'None':
+    if not conversation_labels.desired_state or conversation_labels.desired_state == "None":
         return False
 
     return True
+
 
 class DiaryEntrySummary(BaseModel):
     entry_title: str = Field(
@@ -49,28 +53,28 @@ class DiaryEntrySummary(BaseModel):
         description="A 1 to 2 paragraph summary of the user's experience based on the conversation history between the user and an AI model. Write this in first person perspective."
     )
 
+
 def summarize_new_entry(chat_model):
-    chat_history = ''
+    chat_history = ""
 
     for msg in chat_model.history[2:]:
         chat_history += f"{msg.role}: {msg.parts[0].text} \n\n"
 
     parser = PydanticOutputParser(pydantic_object=DiaryEntrySummary)
-     
-    prompt = f'''
+
+    prompt = f"""
 The following are thoughts from a user, extract the following information.
 {parser.get_format_instructions()}
 ```
 {chat_history}
 ```
-    '''.strip()
-    
+    """.strip()
+
     model = get_llm_instance()
 
-    output = get_completion(model,prompt)
+    output = get_completion(model, prompt)
     parsed_output = parser.parse(output)
-    
+
     print(parsed_output)
-    
+
     return parsed_output
-    
