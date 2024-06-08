93e90d360a78f9fcdc96228b75c9b5c1cfd85482
clara <chongcjy@sph.com.sg>
2024-06-08 14:05:32 +0800

    fixed agent chain

--- agent_chain.py
+++ agent_chain.py
@@ -8,6 +8,7 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from langchain_community.document_loaders import TextLoader
 from langchain_community.vectorstores import FAISS
+from langchain_core.documents import Document
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 
 from old_diary_entries import old_diary_entries
@@ -16,14 +17,34 @@ load_dotenv()
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 
-def initialize_vector_store(old_diary_entries: list):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
-    vector_store = FAISS.from_texts(old_diary_entries, embedding=embeddings)
-    vector_store.save_local("faiss_index")
-    return vector_store
+def add_old_diary_entries_to_db(old_diary_entries: Dict):
+    """
+    Add old diary entries to the vector store
+    """
+    try:
+        embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
+        vector_store = FAISS()
+        list_of_documents = []
 
-
-# initialize_vector_store(old_diary_entries)
+        for _, diary_entry in old_diary_entries.iterrows():
+            list_of_documents.append(
+                Document(
+                    page_content=diary_entry["entry_desc"],
+                    metadata=dict(
+                        entry=diary_entry["entry"],
+                        date=diary_entry["entry_date"],
+                        title=diary_entry["entry_title"],
+                    ),
+                )
+            )
+        vector_store.from_documents(list_of_documents, embeddings)
+        vector_store.save_local("faiss_index")
+        return {
+            "status": "success",
+            "message": "Old diary entries added to the vector store successfully.",
+        }
+    except Exception as e:
+        return {"status": "failure", "message": f"An error occurred: {str(e)}"}
 
 
 def format_diary_entry(diary_entry_path: str, **kwargs):
@@ -39,7 +60,7 @@ def format_diary_entry(diary_entry_path: str, **kwargs):
     return diary_with_metadata
 
 
-def add_diary_to_vector_store(diary_entry: Dict):
+def add_new_diary_to_db(diary_entry: Dict):
     """
     'dairy_entry': should be a path to a .txt file
     Add new diary entries to the vector store
@@ -48,12 +69,9 @@ def add_diary_to_vector_store(diary_entry: Dict):
     """
 
     embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
-    if os.path.exists("faiss_index"):
-        vector_store = FAISS.load_local(
-            "faiss_index", embeddings=embeddings, allow_dangerous_deserialization=True
-        )
-    else:
-        vector_store = FAISS()
+    vector_store = FAISS.load_local(
+        "faiss_index", embeddings=embeddings, allow_dangerous_deserialization=True
+    )
 
     formatted_diary_entry = format_diary_entry(diary_entry)
     vector_store.add_documents(
@@ -117,7 +135,9 @@ def summary_prompts():
     """
     model = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3)
     embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
-    vector_store = FAISS.load_local("faiss_index", embeddings=embeddings, allow_dangerous_deserialization=True)
+    vector_store = FAISS.load_local(
+        "faiss_index", embeddings=embeddings, allow_dangerous_deserialization=True
+    )
     # context is part of the vector store
     past_entries = vector_store.search(" ", search_type="similarity", k=20)
     context = "\n".join(entry.page_content for entry in past_entries)
@@ -136,7 +156,7 @@ def summary_prompts():
 
 
 def get_llm_chat_instance():
-    sys_prompt = '''
+    sys_prompt = """
 You are a therapist psychologist, and you have ability like Chris Voss, the FBI hostage negotiator, who labels what this person is going through.
 
 Based on the ongoing conversations, prompt the user and ask questions to get the following information:
@@ -146,21 +166,16 @@ Based on the ongoing conversations, prompt the user and ask questions to get the
 4. identify a deep-dive question anchored to this situation. Our goal is to understand what's important to them and why.
 5. put all this together. Then make it conversational like Chris Voss.
 (i.e. seems like you were disappointed when xyz. looks like you experienced [current state], while you expected [desired state]. [deep-dive question])
-    '''.strip()
-    
-    model = genai.GenerativeModel('gemini-1.5-flash')
-    chat = model.start_chat(history=[
-        {
-            'role': "user",
-            'parts': [sys_prompt],
-        },
-        {
-            'role': "model",
-            'parts': ["Understood."],
-        },
-    
-    ])
-    
+    """.strip()
+
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    chat = model.start_chat(
+        history=[
+            {"role": "user", "parts": [sys_prompt]},
+            {"role": "model", "parts": ["Understood."]},
+        ]
+    )
+
     return chat
 
 
@@ -168,10 +183,10 @@ def chat_with_user(user_msg, chat_model):
     """
     Takes a user message, creates a response. Will add logic steps to steer the conversation where needed.
     """
-    # To do: to explore streaming 
+    # To do: to explore streaming
     # - https://ai.google.dev/gemini-api/docs/get-started/tutorial?lang=python
     # - https://docs.streamlit.io/develop/api-reference/write-magic/st.write_stream
-    
+
     response = chat_model.send_message(user_msg)
 
     return response.text
